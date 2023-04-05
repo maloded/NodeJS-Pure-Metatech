be77fc3034991e8b8aea515ab486cef2a7d20e49
maloded <damatomxxx@gmail.com>
2023-04-05 15:47:06 +0300

    Optimize loader: add loadDir

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const fsp = require('node:fs').promises;
-const path = rquire('node:path');
-
+const path = require('node:path');
 const staticServer = require('./lib/static.js');
 const logger = require('./lib/logger.js');
 const common = require('./lib/common.js');
-const loadConfig = require('./lib/config.js');
-const load = require('./lib/load.js');
+const { loadDir } = require('./lib/load.js');
 const ws = require('./lib/ws.js');
 
 const appPath = path.join(process.cwd(), '../NodeJS-Application-Metatech');
@@ -21,21 +18,13 @@ const staticPath = path.join(appPath, './static');
         common: Object.freeze(common),
     }
 
-    const config = await loadConfig(configPath, sandbox);
+    const config = await loadDir(configPath, sandbox);
     const db = require('./lib/db.js')(config.db);
 
     sandbox.api = Object.freeze({});
     sandbox.db = Object.freeze(db);
 
-    const routing = {};
-
-    const files = await fsp.readdir(apiPath);
-    for (const fileName of files) {
-        if (!fileName.endsWith('.js')) continue;
-        const filePath = path.join(apiPath, fileName);
-        const serviceName = path.basename(fileName, '.js');
-        routing[serviceName] = await load(filePath, sandbox);
-    }
+    const routing = loadDir(apiPath, sandbox);
 
     const [port] = config.server.ports;
     const server = staticServer(staticPath, port, logger);
